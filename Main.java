fcd0c4d30020d0715e4af2c13bf83e1e2d2edda0
Mudimba Moonde <mudimbamoonde@gmail.com>
2020-11-11 21:39:01 +0200

    Had to import java.util.*; Classes

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
+import java.util.*;
 public class Main{
     public static void main(String[] args) {
         ArrayList<String> names = new ArrayList<String>();
-        names.add(1,"Mudimba");
+        names.add("Mudimba");
 
         for(String na:names){
             System.out.println("[+] " + na);
