ce720d12e0244831f06ef69c88b00e81518ef9f0
chimz95 <69400307+chimz95@users.noreply.github.com>
2020-11-11 11:25:04 -0800

    ArrayList<String> names = new ArrayList<String>();
    You need to add an extra <String> in the space constructor.

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[] args) {
-        ArrayList<String> names = new ArrayList<>();
+        ArrayList<String> names = new ArrayList<String>();
         names.add(1,"Mudimba");
 
         for(String na:names){
