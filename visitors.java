4a396df2e79e0bb0291deb73a10cb8c396f38519
Chitumbi Peter <chitumbipeter@gmail.com>
2020-11-11 21:55:15 -0800

    Create a Vistor class

---
+++ visitors.java
@@ -0,0 +1,32 @@
+public class visitors{
+    private String FirstName;
+    private String LastName;
+    private int Age;
+
+
+
+    public String getFirstName(){
+       return FirstName;
+    }
+   
+    public void setFirstName(String fn){
+        this.FirstName = fn;
+    }
+
+    public String getLastName(){
+        return LastName;
+     }
+    
+     public void setLastName(String ln){
+         this.LastName = ln;
+     }
+
+     public String getAge(){
+        return Age;
+     }
+    
+     public void setAge(String age){
+         this.Age = age;
+     }
+
+}
