e69647d9876443ebf692f0c9c954a4e9a86b8601
Mudimba Moonde <mudimbamoonde@gmail.com>
2020-11-14 22:53:24 +0200

    Python Script

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
 import java.util.*;
 public class Main{
     public static void main(String[] args) {
+        String [] names  = new String[20];
+        
         ArrayList<String> names = new ArrayList<String>();
         names.add("Mudimba");
+        names.add("Lang");
+        names.add("Tech");
+        // names.add(21);
 
+        ArrayList<String> view = new ArrayList<String>();
         for(String na:names){
             System.out.println("[+] " + na);
         }
---
+++ main.py
@@ -0,0 +1,8 @@
+List = ["Name","Lang","Tech",12]
+
+tup = []
+for x in List:
+  tup.append(x)   
+# print(x)
+
+print(tup)
